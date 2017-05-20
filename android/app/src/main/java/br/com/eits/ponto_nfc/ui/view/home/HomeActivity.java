8c190599a7085a64e70fae846438426843fb9822
Victor Carvalho <victor.blq@gmail.com>
2017-05-20 18:02:56 -0300

    Feign + Jackson

---
+++ android/app/src/main/java/br/com/eits/ponto_nfc/ui/view/home/HomeActivity.java
@@ -0,0 +1,18 @@
+package br.com.eits.ponto_nfc.ui.view.home;
+
+import android.speech.tts.TextToSpeech;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.EActivity;
+
+import java.util.Locale;
+
+import br.com.eits.ponto_nfc.R;
+
+@EActivity(R.layout.activity_ponto)
+public class HomeActivity extends AppCompatActivity
+{
+    
+}
