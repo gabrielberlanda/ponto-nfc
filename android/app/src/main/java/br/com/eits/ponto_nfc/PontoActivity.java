fafcdbebc6cfcbfe5a1743851f34b00ac446f0c5
Victor Carvalho <victor.blq@gmail.com>
2017-05-20 16:59:53 -0300

    configuração android annotations

--- android/app/src/main/java/br/com/eits/ponto_nfc/PontoActivity.java
+++ android/app/src/main/java/br/com/eits/ponto_nfc/PontoActivity.java
@@ -1,13 +1,16 @@
 package br.com.eits.ponto_nfc;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class PontoActivity extends AppCompatActivity {
+import org.androidannotations.annotations.EActivity;
 
+@EActivity(R.layout.activity_ponto)
+public class PontoActivity extends AppCompatActivity {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_ponto);
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        
     }
 }
