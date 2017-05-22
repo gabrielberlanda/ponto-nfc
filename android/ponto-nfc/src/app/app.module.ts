0ef046caf0b4c1bb8b51e8ee3158878e067100d5
Gabriel Berlanda <gabriel.berlanda@gmail.com>
2017-05-22 13:20:32 -0300

    Adeus android, Olá Ionic

---
+++ android/ponto-nfc/src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NFC , Ndef} from '@ionic-native/nfc';
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    NFC,
+    Ndef,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
